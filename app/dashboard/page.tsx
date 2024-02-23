ee4de5b5ffb9ae4b89baa912116279133bbdb6d2
Lukasfenkart <90911645+Lukasfenkart@users.noreply.github.com>
2024-02-23 15:52:58 +0100

    dashboard anfang

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,30 +1,60 @@
-import Image from 'next/image'
-
+import Image from "next/image";
 
 export default function Dashboard() {
   return (
     <div className=" bg-slate-100 h-full p-20 bg-center">
       <div className="bg-white p-16 rounded-3xl">
-        <div className='flex flex-row justify-between pb-3'>
+        <div className="flex flex-row justify-between">
           <div>
-          <h1 className="text-3xl pb-3">Autoverleih Dashboard</h1>
+            <h1 className="text-3xl pb-3">Autoverleih Dashboard</h1> <hr />
           </div>
-          <div className='bg-slate-800 text-white p-3 rounded-xl'>
-            <h2 className='text-xl'> Logindata: </h2><br/> <hr className='bg-white'/>
-            <div className='p-2'>
-            lfenkart <br/>
-            lukasfenkart1011@gmail.com
+          <div className="bg-slate-500 text-white p-3 rounded-xl">
+            <h2 className="text-xl"> Logindata: </h2>
+            <br /> <hr className="bg-white" />
+            <div className="p-2">
+              lfenkart <br />
+              lukasfenkart1011@gmail.com
             </div>
           </div>
         </div>
-          <hr />
-      </div>
 
-      <div className='bg-slate-200'>
-        <div>
+        <div className="p-3">
+          <hr />
+        </div>
+
+        <div className="bg-slate-200 rounded-t-xl">
+          <div className="p-2">
+            <button className="p-2 bg-slate-300 rounded-lg hover:bg-slate-400">
+              search for free cars
+            </button>
+          </div>
+        </div>
+        
+        <div className="bg-slate-300 rounded-b-xl">
+          <div className="p-5">
+            Auto
+            <div>
+              <hr />
+            </div>
+          </div>
+          
+          <div className="p-5">
+            Auto
+            <div>
+              <hr />
+            </div>
+          </div>
+
+          <div className="p-5">
+            Auto
+            <div>
+              <hr />
+            </div>
+          </div>
           
         </div>
+
       </div>
     </div>
-  )
+  );
 }
