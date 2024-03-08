6a8c5cc9755f99a3867edf40c043e3c562867599
Lukasfenkart <90911645+Lukasfenkart@users.noreply.github.com>
2024-03-08 14:12:07 +0100

    aktueller Stand Lukas

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,15 @@
+import Inputfield from "@/components/inputfield";
 import Image from "next/image";
 
 export default function Dashboard() {
   return (
-    <div className=" bg-slate-100 h-full p-20 bg-center">
+    <div className=" bg-slate-100 h-full py-20 px-40 bg-center">
       <div className="bg-white p-16 rounded-3xl">
         <div className="flex flex-row justify-between">
           <div>
-            <h1 className="text-3xl pb-3">Autoverleih Dashboard</h1> <hr />
+            <h1 className="text-3xl pb-3">CRD - Car Rent Dashboard</h1> <hr />
           </div>
-          <div className="bg-slate-500 text-white p-3 rounded-xl">
+          <div className="bg-slate-400 text-white p-3 rounded-xl">
             <h2 className="text-xl"> Logindata: </h2>
             <br /> <hr className="bg-white" />
             <div className="p-2">
@@ -18,42 +19,47 @@ export default function Dashboard() {
           </div>
         </div>
 
-        <div className="p-3">
+        <div className="py-3">
           <hr />
         </div>
 
-        <div className="bg-slate-200 rounded-t-xl">
-          <div className="p-2">
-            <button className="p-2 bg-slate-300 rounded-lg hover:bg-slate-400">
+        <div className="bg-slate-100 rounded-t-xl text-lg">
+          <div className="p-5 flex flex-row justify-between">
+            <button className="p-2 px-4 bg-slate-200 rounded-lg hover:bg-slate-300">
               search for free cars
             </button>
+
+            <div className="flex flex-row gap-5">
+              <div className="flex flex-row p-1">
+                <div className="p-1 px-4 rounded-l-xl bg-white">Start Date: </div>
+                <div>
+                  <input
+                    type="date"
+                    className="p-1 bg-slate-150 rounded-r-xl hover:bg-slate-200"
+                  />
+                </div>
+              </div>
+
+              <div className="flex flex-row p-1">
+                <div className="p-1 px-4 rounded-l-xl bg-white">End Date: </div>
+                <div>
+                  <input
+                    type="date"
+                    className="p-1 bg-white rounded-r-xl hover:bg-slate-200"
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        
-        <div className="bg-slate-300 rounded-b-xl">
-          <div className="p-5">
-            Auto
-            <div>
-              <hr />
-            </div>
-          </div>
-          
-          <div className="p-5">
-            Auto
-            <div>
-              <hr />
-            </div>
-          </div>
 
-          <div className="p-5">
-            Auto
-            <div>
-              <hr />
-            </div>
-          </div>
-          
+        <div className="bg-slate-200 rounded-b-xl">
+          <div className="p-5">Test Auto 1</div>
+
+          <div className="p-5">Test Auto 2</div>
+
+          <div className="p-5">Test Auto 3</div>
         </div>
-
       </div>
     </div>
   );
--- components/loginbutton.tsx
+++ components/loginbutton.tsx
@@ -5,7 +5,7 @@ interface Loginbuttonprops {
 
 const Loginbutton = (props: Loginbuttonprops) => {
   return (
-    <div className="pt-10 pl-5">
+    <div className="pt-10 pr-10">
       <button className="py-3 px-5 bg-blue-600 rounded-xl text-white hover:bg-blue-800" onClick={props.onClick}>
       <div>{props.name}</div>
       </button>
